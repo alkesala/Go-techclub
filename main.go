0a035a7b5871ba71c739adff4fe67984382de0a9
Aleksi Kesälä <nordicdevbox@proton.me>
2025-10-31 17:15:35 +0200

    feat: handlers and hello-go

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "lets gooooo %s", time.Now())
+}
+
+func buzz(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "buzzzzzzzz")
+}
+
+var port = 8080
+
 func main() {
+	http.HandleFunc("/", greet)
+	http.HandleFunc("/buzz", buzz)
+	fmt.Printf("The server is up n running on %d\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
