4d540c9c96ffaf4db01e9d734bdbc788f6ebabd8
Aleksi Kesälä <nordicdevbox@proton.me>
2025-10-31 17:23:24 +0200

    feat: added "error handling"

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "lets gooooo %s", time.Now())
+	_, _ = fmt.Fprintf(w, "lets gooooo %s", time.Now())
 }
 
 func buzz(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "buzzzzzzzz")
+	_, _ = fmt.Fprintf(w, "buzzzzzzzz")
 }
 
 var port = 8080
@@ -20,5 +20,5 @@ func main() {
 	http.HandleFunc("/", greet)
 	http.HandleFunc("/buzz", buzz)
 	fmt.Printf("The server is up n running on %d\n", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	_ = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
