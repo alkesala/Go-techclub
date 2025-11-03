f28c88f97f6afff68ae4568c6efe462a1c02761f
Aleksi Kesälä <nordicdevbox@proton.me>
2025-11-03 18:44:14 +0200

    feat: added healthcheck

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 )
 
+type Response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
 func greet(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, "lets gooooo %s", time.Now())
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	response := Response{
+		Status: "OK",
+	}
+	_ = json.NewEncoder(w).Encode(response)
+
 }
 
 func buzz(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +35,7 @@ var port = 8080
 
 func main() {
 	http.HandleFunc("/", greet)
+	http.HandleFunc("/health", health)
 	http.HandleFunc("/buzz", buzz)
 	fmt.Printf("The server is up n running on %d\n", port)
 	_ = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
