07d1e5a227fb78b2fc99d416b08ea33777ee06b0
Aleksi Kesälä <nordicdevbox@proton.me>
2025-11-10 11:01:49 +0200

    feat: brrr handler

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
+
+func brrr(w http.ResponseWriter, _ *http.Request) {
+	_, _ = fmt.Fprintf(w, "Brrrrrrrrrr %s", time.Now())
+}
 
 var port = 8080 // common development port for Go
 func main() {
-	fmt.Printf("The server is running on: %d", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	http.HandleFunc("/brrr", brrr)
+	fmt.Printf("The server is running on: %d\n", port)
+	_ = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 
 }
