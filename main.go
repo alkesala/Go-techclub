ada2550e967a201216d9fa899594ce79c0cfba13
Aleksi Kesälä <nordicdevbox@proton.me>
2025-11-10 10:51:37 +0200

    feat: basic server up n running

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+var port = 8080 // common development port for Go
 func main() {
+	fmt.Printf("The server is running on: %d", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
 }
