0a8737f4c873e6a9afe19ba38cb11e8355894c92
Aleksi Kesälä <nordicdevbox@proton.me>
2025-11-12 08:33:16 +0200

    feat: the starter template

--- main.go
+++ main.go
@@ -1,47 +1,21 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"time"
-)
+// Response struct
+// .........
 
-// omitempty == optional
+// Health check
+// ....
 
-type Response struct {
-	Status  string `json:"status"`
-	Message string `json:"message,omitempty"`
-}
+// endpoint 1
+// ....
 
-func health(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	resp := Response{
-		Status: "OK",
-	}
-	if err := json.NewEncoder(w).Encode(resp); err != nil {
-		log.Printf("Error encoding json %s", err)
-	}
+// endpoint 2
+//...
 
-}
+// Port config
 
-func brrr(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "Brrrrrrrrrr %s", time.Now())
-}
-
-func buzz(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "There is alot buzzwords in the IT sector")
-}
-
-var port = 8080 // common development port for Go
 func main() {
-
-	http.HandleFunc("/brrr", brrr)
-	http.HandleFunc("/buzz", buzz)
-	http.HandleFunc("/health", health)
-	fmt.Printf("The server is running on: %d\n", port)
-	_ = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	// register routes
+	// start the server
 
 }
